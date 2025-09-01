0edb253feaba8e03f5c8a4ffed24e400bd4a13c2
Kajal Talwekar <kajaltalwekar02@gmail.com>
2025-09-01 22:55:28 +0530

    navbar in progress

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,116 +1,84 @@
 // -------------------------------------------------------------------------------------------
 
-import styles from './Header.module.scss';
+import styles from "./Header.module.scss";
 
-import { useState } from 'react';
-import IconButton from '../common-buttons/IconButton/IconButton';
-import NavigationButton from '../common-buttons/NavigationButton/NavigationButton';
-import { CloseIcon, MenuIcon } from '../Icons/Icons';
+import { useState } from "react";
+import IconButton from "../common-buttons/IconButton/IconButton";
+import NavigationButton from "../common-buttons/NavigationButton/NavigationButton";
+import { CloseIcon, MenuIcon } from "../Icons/Icons";
 
 // -------------------------------------------------------------------------------------------
 
 const routes = [
-	{ label: 'Introduction', link: '#introduction' },
-	{ label: 'Skills', link: '#skills' },
-	{ label: 'Experience', link: '#experience' },
-	{ label: 'Projects', link: '#projects' },
-	{ label: 'Education', link: '#education' },
-	{ label: 'Thanks', link: '#thanks' },
-	{ label: 'Contact', link: '#contact' },
+  { label: "Introduction", link: "#introduction" },
+  { label: "Skills", link: "#skills" },
+  { label: "Experience", link: "#experience" },
+  { label: "Projects", link: "#projects" },
+  { label: "Education", link: "#education" },
+  { label: "Thanks", link: "#thanks" },
+  { label: "Contact", link: "#contact" },
 ];
 
 // -------------------------------------------------------------------------------------------
 
 const Header = () => {
-	const [navigationScreen, setNavigationScreen] = useState(false);
+  const [navigationScreen, setNavigationScreen] = useState(false);
 
-	const handleNavigationScreenToggle = () => {
-		setNavigationScreen((prev) => !prev);
-	};
+  const handleNavigationScreenToggle = () => {
+    setNavigationScreen((prev) => !prev);
+  };
 
-	return (
-		<>
-			<div
-				
-				className={styles.container}
-			>
-				<div
-					
-					className={styles.frame}
-				>
-					<div
-						
-						className={styles.leading}
-					>
-						<h1
-							
-							className={styles.headlinetitlelarge}
-						>
-							Portfolio
-						</h1>
-					</div>
-					<div
-						
-						className={styles.trailing}
-					>
-						<div
-							
-							className={styles.navigationbuttonscontainer}
-						>
-							{routes.map((button, index) => {
-								return (
-									<NavigationButton
-										key={`navigation-button-${index}`}
-										{...button}
-									/>
-								);
-							})}
-						</div>
-						<div
-							
-							className={styles.iconbuttoncontainer}
-						>
-							<IconButton
-								icon={<MenuIcon />}
-								onClick={handleNavigationScreenToggle}
-							/>
-						</div>
-					</div>
-				</div>
-			</div>
-			{navigationScreen && (
-				<div
-					
-					className={styles.navigationscreen}
-				>
-					<div
-						
-						className={styles.top}
-					>
-						<IconButton
-							icon={<CloseIcon />}
-							onClick={handleNavigationScreenToggle}
-						/>
-					</div>
-					<div
-						
-						className={styles.bottom}
-					>
-						{routes.map((button, index) => {
-							return (
-								<div
-									key={`navigation-button-${index}`}
-									onClick={handleNavigationScreenToggle}
-								>
-									<NavigationButton {...button} />
-								</div>
-							);
-						})}
-					</div>
-				</div>
-			)}
-		</>
-	);
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.frame}>
+          <div className={styles.leading}>
+            <h1 className={styles.headline}>Portfolio</h1>
+          </div>
+          <div className={styles.trailing}>
+            <div className={styles.navigationbuttonscontainer}>
+              {routes.map((button, index) => {
+                return (
+                  <NavigationButton
+                    key={`navigation-button-${index}`}
+                    {...button}
+                  />
+                );
+              })}    
+            </div>
+            <div className={styles.iconbuttoncontainer}>
+              <IconButton
+                icon={<MenuIcon />}
+                onClick={handleNavigationScreenToggle}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      {navigationScreen && (
+        <div className={styles.navigationscreen}>
+          <div className={styles.top}>
+            <IconButton
+              icon={<CloseIcon />}
+              onClick={handleNavigationScreenToggle}
+            />
+          </div>
+          <div className={styles.bottom}>
+            {routes.map((button, index) => {
+              return (
+                <div
+                  key={`navigation-button-${index}`}
+                  onClick={handleNavigationScreenToggle}
+                >
+                  <NavigationButton {...button} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 // -------------------------------------------------------------------------------------------
