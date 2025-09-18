203d44b5271229abad3b9238dc3c30419a0f8eba
Kajal Talwekar <kajaltalwekar02@gmail.com>
2025-09-18 23:44:14 +0530

    feat(skill): create skill card layout structure

--- src/sections/Skills/Skills.tsx
+++ src/sections/Skills/Skills.tsx
@@ -2,7 +2,7 @@
 
 import type { FC, JSX } from 'react';
 import styles from './Skills.module.scss';
-
+import htmlLogo from '../../assets/logos/html_logo.png';
 // ------------------------------------------------------------------------------------------
 
 const Skills: FC = (): JSX.Element => {
@@ -10,7 +10,21 @@ const Skills: FC = (): JSX.Element => {
         <div className={styles.container}>
             <h2 className={styles.headline}>Skills</h2>
             <div className={styles.content}>
-                <div className={styles.box}>Box 1</div>
+                <div className={styles.box}>
+                    <div className={styles.box_content}>
+                        <div className={styles.lang_logo}>
+                            <img
+                                src={htmlLogo}
+                                alt=''
+                            />
+                            <p className={styles.lang_text}>HTML</p>
+                        </div>
+                        <div className={styles.description}>
+                            <p>Standard markup language for structuring web content.</p>
+                        </div>
+                        <div className={styles.domain}>Frontend</div>
+                    </div>
+                </div>
                 <div className={styles.box}>Box 2</div>
                 <div className={styles.box}>Box 3</div>
                 <div className={styles.box}>Box 4</div>
